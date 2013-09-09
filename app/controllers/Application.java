8360c7e6393dbf37a64b03da5d83140cc3e6cfbb
doruk <itolia02@itolia.com>
2013-09-10 01:26:47 +0300

    session variables

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -45,32 +45,23 @@ import com.isurveysoft.www.servicesv5.Survey;
 import com.isurveysoft.www.servicesv5.SurveyResult;
 
 public class Application extends Controller {
-	private static Map<Long, Integer> chartQuestions = new HashMap<Long, Integer>();
-	private static Map<Long, Integer> descQuestions = new HashMap<Long, Integer>();
+//	private static Map<Long, Integer> chartQuestions = new HashMap<Long, Integer>();
+//	private static Map<Long, Integer> descQuestions = new HashMap<Long, Integer>();
 	private static Map<Integer,Integer> mahalleilce = new HashMap<Integer, Integer>();
 	private static List<Integer> allDistricts = java.util.Collections.synchronizedList(new ArrayList<Integer>());
-	private static String user;
-	private static String surveyPin;
-	private static String surveyCP;
-	private static long filterScreenId;
-	private static int surveyMainLocId;
-	private static int surveyUndefinedTownId;
-	private static int surveyMainLocType;
-	private static int surveyId;
 	private static Date fromDate;
 	private static Date toDate;
 
 	public static Result wsdl() throws RemoteException, ServiceException {
 		ExportService service = new ExportServiceLocator();
-		Survey survey = service.getExportServiceSoap().exportSurvey(surveyCP,
-				surveyPin);
+		Survey survey = service.getExportServiceSoap().exportSurvey(session("surveyCP"),session("surveyPin"));
 		Screen[] screens = survey.getScreens();
 		return ok(Json.toJson(screens));
 	}
 	public static Result qdb() throws ServiceException, SQLException, IOException {
 		ExportService service = new ExportServiceLocator();
-		Survey survey = service.getExportServiceSoap().exportSurvey(surveyCP,
-				surveyPin);
+		Survey survey = service.getExportServiceSoap().exportSurvey(session("surveyCP"),
+				session("surveyPin"));
 		Screen[] screens = survey.getScreens();
 		Statement st = DB.getConnection().createStatement();
 		String sql="insert into xscreen values ";
@@ -105,7 +96,7 @@ public class Application extends Controller {
 
 	public static Result ans() throws RemoteException, ServiceException {
 		ExportService service = new ExportServiceLocator();
-		SurveyResult[] results = service.getExportServiceSoap().exportSurveyResults(surveyCP, surveyPin,fromDate.toString(), toDate.toString(), 0l);
+		SurveyResult[] results = service.getExportServiceSoap().exportSurveyResults(session("surveyCP"), session("surveyPin"),fromDate.toString(), toDate.toString(), 0l);
 		return ok(Json.toJson(results));
 	}
 	private static class Facebook implements Callable<SurveyResult[]>{
@@ -118,7 +109,7 @@ public class Application extends Controller {
 		@Override
 		public SurveyResult[] call() throws Exception {
 			System.out.println(dt.toString()+" ------ "+ dt.plusDays(1).toString());
-			return service.getExportServiceSoap().exportSurveyResults(surveyCP, surveyPin,dt.toString(), dt.plusDays(1).toString(), 0l);
+			return service.getExportServiceSoap().exportSurveyResults(session("surveyCP"), session("surveyPin"),dt.toString(), dt.plusDays(1).toString(), 0l);
 		}
 	}
 	public static Result adb() throws ServiceException, SQLException, IOException, InterruptedException, ExecutionException {
@@ -136,13 +127,13 @@ public class Application extends Controller {
 	    executor.shutdown();
 	    Statement st =DB.getConnection().createStatement();
 		int c = 0;
-	    for (Future<SurveyResult[]> future : list) {
+	    for (Future<SurveyResult[]> future : list){
 	    	SurveyResult[] results = future.get();
 	    	System.out.println(results.length);
 			List<String> extracted = extracted(results,st);
 			for (String sql : extracted) {
 				st.addBatch(sql);
-				if(++c%1000 ==0)
+				if(++c%2000 ==0)
 					st.executeBatch();
 			}
 	    }
@@ -155,7 +146,7 @@ public class Application extends Controller {
 		if(results==null||results.length<1)
 			return new LinkedList<String>();
 		Statement st = a;
-		ResultSet rs = st.executeQuery("select filterscreenid from survey where id = "+ surveyId);
+		ResultSet rs = st.executeQuery("select filterScreenId from survey where id = "+ session("surveyId"));
 		rs.next();
 		long districtId = rs.getLong(1);
 		List<String> l = new LinkedList<String>();
@@ -180,34 +171,34 @@ public class Application extends Controller {
 			throws RemoteException, ServiceException, SQLException {
 		ExportService service = new ExportServiceLocator();
 
-		Screen[] cachedScreens = (Screen[]) Cache.get(user + "screen");
+		Screen[] cachedScreens = (Screen[]) Cache.get(session("user") + "screen");
 		if (cachedScreens == null || cachedScreens.length < 1) {
-			Survey survey = service.getExportServiceSoap().exportSurvey(surveyCP, surveyPin);
+			Survey survey = service.getExportServiceSoap().exportSurvey(session("surveyCP"), session("surveyPin"));
 			cachedScreens = survey.getScreens();
-			Cache.set(user + "screen", cachedScreens);
+			Cache.set(session("user") + "screen", cachedScreens);
 		}
-		SurveyResult[] cachedResults = (SurveyResult[]) Cache.get(user+ "answer");
+		SurveyResult[] cachedResults = (SurveyResult[]) Cache.get(session("user")+ "answer");
 		if (cachedResults == null || cachedResults.length < 1) {
-			cachedResults = service.getExportServiceSoap().exportSurveyResults(surveyCP, surveyPin,fromDate.toString(), toDate.toString(), 0l);
-			//			cachedResults = service.getExportServiceSoap().exportSurveyResults(surveyCP, surveyPin, "2013-08-22 15:11:42","2013-09-20 00:11:42", 0l);
-			Cache.set(user + "answer", cachedResults);
+			cachedResults = service.getExportServiceSoap().exportSurveyResults(session("surveyCP"), session("surveyPin"),fromDate.toString(), toDate.toString(), 0l);
+			//			cachedResults = service.getExportServiceSoap().exportSurveyResults(session("surveyCP"), session("surveyPin"), "2013-08-22 15:11:42","2013-09-20 00:11:42", 0l);
+			Cache.set(session("user") + "answer", cachedResults);
 		}
-		return ok(Json.toJson(toJsonFormat(cachedScreens, cachedResults, town,
-				district)));
+		return ok("1");
+//		return ok(Json.toJson(toJsonFormat(cachedScreens, cachedResults, town,district)));
 	}
 	public static Result clearCache(){
-		Cache.remove(user+"screen");
-		Cache.remove(user+"answer");
+		Cache.remove(session("user")+"screen");
+		Cache.remove(session("user")+"answer");
 		return ok("");
 	}
 	public static Result geoResults(long screenId) throws RemoteException, ServiceException {
 		ExportService service = new ExportServiceLocator();
 		@SuppressWarnings("unchecked")
-		List<GeoAnswerJSON> cached = (List<GeoAnswerJSON>) Cache.get(user+ "geo" + screenId);
+		List<GeoAnswerJSON> cached = (List<GeoAnswerJSON>) Cache.get(session("user")+ "geo" + screenId);
 		if (cached == null || cached.size() < 1) {
-			SurveyResult[] results = service.getExportServiceSoap().exportSurveyResults(surveyCP, surveyPin,fromDate.toString(), toDate.toString(), 0l);
+			SurveyResult[] results = service.getExportServiceSoap().exportSurveyResults(session("surveyCP"), session("surveyPin"),fromDate.toString(), toDate.toString(), 0l);
 			cached = toGeoJsonFormat(screenId, results);
-			Cache.set(user + "geo" + screenId, cached);
+			Cache.set(session("user") + "geo" + screenId, cached);
 		}
 		return ok(Json.toJson(cached));
 	}
@@ -229,7 +220,7 @@ public class Application extends Controller {
 //		for (Resultx sr : results) {
 //			boolean onRegion = true;
 //			if (town > 0)
-//				if (sr.getScreenId() == filterScreenId&& sr.getText() != null&& sr.getText().length() > 0) {
+//				if (sr.getScreenId() == session("filterScreenId")&& sr.getText() != null&& sr.getText().length() > 0) {
 //					String clearedFromChars = sr.getText().replaceAll("[\\D]", "").replaceFirst ("^0*", "");
 //					if(clearedFromChars.length()==0||clearedFromChars.length()>3){
 //						onRegion=false;
@@ -238,9 +229,9 @@ public class Application extends Controller {
 //					allDistricts.add(Integer.parseInt(clearedFromChars));
 //					if (district > 0) {
 //						onRegion = district==Integer.parseInt(clearedFromChars);
-//					} else if(surveyMainLocType!=2){
+//					} else if(session("surveyMainLocType")!=2){
 //						Integer townofdistrict = mahalleilce.get(Integer.parseInt(clearedFromChars));
-//						onRegion = town == (townofdistrict==null?surveyUndefinedTownId:townofdistrict);
+//						onRegion = town == (townofdistrict==null?session("surveyUndefinedTownId"):townofdistrict);
 //					}
 //				}
 //			if (onRegion){
@@ -305,92 +296,92 @@ public class Application extends Controller {
 //		list.addAll(listMultiQue);
 		return list;
 	}
-	private static List<QuestionJSON> toJsonFormat(Screen[] screens,SurveyResult[] results, int town, int district) {
-		List<QuestionJSON> list = new LinkedList<QuestionJSON>();
-		List<QuestionJSON> listMultiQue = new LinkedList<QuestionJSON>();
-		HashMap<Tuple<Long, Long>, Integer> ansMap = new HashMap<Tuple<Long, Long>, Integer>();
-		for (SurveyResult sr : results) {
-			boolean onRegion = true;
-			if (town > 0)
-				for (com.isurveysoft.www.servicesv5.Result res : sr.getScreenResults()) {
-					if (res.getScreenId() == filterScreenId&& res.getResultAnswer() != null&& res.getResultAnswer().length() > 0) {
-						String clearedFromChars = res.getResultAnswer().replaceAll("[\\D]", "").replaceFirst ("^0*", "");
-						if(clearedFromChars.length()==0||clearedFromChars.length()>3){
-							onRegion=false;
-							break;
-						}
-						allDistricts.add(Integer.parseInt(clearedFromChars));
-						if (district > 0) {
-							onRegion = district==Integer.parseInt(clearedFromChars);
-						} else if(surveyMainLocType!=2){
-							Integer townofdistrict = mahalleilce.get(Integer.parseInt(clearedFromChars));
-							onRegion = town == (townofdistrict==null?surveyUndefinedTownId:townofdistrict);
-						}
-					}
-				}
-			if (onRegion)
-				for (com.isurveysoft.www.servicesv5.Result res : sr.getScreenResults()) {
-					Tuple<Long, Long> tuple = new Tuple<Long, Long>(res.getQuestionId() == null ? res.getScreenId(): res.getQuestionId(), res.getAnswerId());
-					Integer c = ansMap.get(tuple);
-					if (c == null)
-						ansMap.put(tuple, 1);
-					else
-						ansMap.put(tuple, c + 1);
-				}
-		}
-		for (Screen screen : screens) {
-			if (!chartQuestions.containsKey(screen.getScreenId())) {
-				continue;
-			}
-			HashMap<Long, String> ansTextMap = new HashMap<Long, String>();
-			for (Answer answer : screen.getAnswers()) {
-				ansTextMap.put(answer.getAnswerId(), answer.getAnswerText());
-			}
-			if (screen.getQuestions() != null
-					&& screen.getQuestions().length > 0) {
-				for (Question q : screen.getQuestions()) {
-					QuestionJSON que = new QuestionJSON();
-					que.setScreenId(screen.getScreenId());
-					que.setText(q.getQuestionText());
-					que.setType(3);
-					List<AnswerJSON> ansList = new LinkedList<AnswerJSON>();	
-					for(Long x : ansTextMap.keySet()){
-						Tuple<Long, Long> key = new Tuple<Long,Long>(q.getQuestionId(),x);
-						AnswerJSON ans = new AnswerJSON();
-						ans.setCode(x);
-						ans.setText(ansTextMap.get(x));
-						Integer count = ansMap.get(key);
-						ans.setCount(count == null ? 0 : count);
-						if (count != null)
-							ansList.add(ans);
-					}
-					que.setAnswer(ansList);
-					listMultiQue.add(que);
-				}
-			} else {
-				QuestionJSON que = new QuestionJSON();
-				que.setText(screen.getScreenText());
-				que.setScreenId(screen.getScreenId());
-				que.setType(chartQuestions.get(screen.getScreenId()));
-				List<AnswerJSON> ansList = new LinkedList<AnswerJSON>();
-				for (Long x : ansTextMap.keySet()) {
-					Tuple<Long, Long> key = new Tuple<Long, Long>(screen.getScreenId(), x);
-					AnswerJSON ans = new AnswerJSON();
-					ans.setCode(x);
-					ans.setText(ansTextMap.get(x));
-					Integer count = ansMap.get(key);
-					ans.setCount(count == null ? 0 : count);
-					if (count != null)
-						ansList.add(ans);
-				}
-				que.setAnswer(ansList);
-				list.add(que);
-			}
-
-		}
-		list.addAll(listMultiQue);
-		return list;
-	}
+//	private static List<QuestionJSON> toJsonFormat(Screen[] screens,SurveyResult[] results, int town, int district) {
+//		List<QuestionJSON> list = new LinkedList<QuestionJSON>();
+//		List<QuestionJSON> listMultiQue = new LinkedList<QuestionJSON>();
+//		HashMap<Tuple<Long, Long>, Integer> ansMap = new HashMap<Tuple<Long, Long>, Integer>();
+//		for (SurveyResult sr : results) {
+//			boolean onRegion = true;
+//			if (town > 0)
+//				for (com.isurveysoft.www.servicesv5.Result res : sr.getScreenResults()) {
+//					if (res.getScreenId() == Long.parseLong(session("filterScreenId"))&& res.getResultAnswer() != null&& res.getResultAnswer().length() > 0) {
+//						String clearedFromChars = res.getResultAnswer().replaceAll("[\\D]", "").replaceFirst ("^0*", "");
+//						if(clearedFromChars.length()==0||clearedFromChars.length()>3){
+//							onRegion=false;
+//							break;
+//						}
+//						allDistricts.add(Integer.parseInt(clearedFromChars));
+//						if (district > 0) {
+//							onRegion = district==Integer.parseInt(clearedFromChars);
+//						} else if(Integer.parseInt(session("surveyMainLocType"))!=2){
+//							Integer townofdistrict = mahalleilce.get(Integer.parseInt(clearedFromChars));
+//							onRegion = town == (townofdistrict==null?Integer.parseInt(session("surveyUndefinedTownId")):townofdistrict);
+//						}
+//					}
+//				}
+//			if (onRegion)
+//				for (com.isurveysoft.www.servicesv5.Result res : sr.getScreenResults()) {
+//					Tuple<Long, Long> tuple = new Tuple<Long, Long>(res.getQuestionId() == null ? res.getScreenId(): res.getQuestionId(), res.getAnswerId());
+//					Integer c = ansMap.get(tuple);
+//					if (c == null)
+//						ansMap.put(tuple, 1);
+//					else
+//						ansMap.put(tuple, c + 1);
+//				}
+//		}
+//		for (Screen screen : screens) {
+//			if (!chartQuestions.containsKey(screen.getScreenId())) {
+//				continue;
+//			}
+//			HashMap<Long, String> ansTextMap = new HashMap<Long, String>();
+//			for (Answer answer : screen.getAnswers()) {
+//				ansTextMap.put(answer.getAnswerId(), answer.getAnswerText());
+//			}
+//			if (screen.getQuestions() != null
+//					&& screen.getQuestions().length > 0) {
+//				for (Question q : screen.getQuestions()) {
+//					QuestionJSON que = new QuestionJSON();
+//					que.setScreenId(screen.getScreenId());
+//					que.setText(q.getQuestionText());
+//					que.setType(3);
+//					List<AnswerJSON> ansList = new LinkedList<AnswerJSON>();	
+//					for(Long x : ansTextMap.keySet()){
+//						Tuple<Long, Long> key = new Tuple<Long,Long>(q.getQuestionId(),x);
+//						AnswerJSON ans = new AnswerJSON();
+//						ans.setCode(x);
+//						ans.setText(ansTextMap.get(x));
+//						Integer count = ansMap.get(key);
+//						ans.setCount(count == null ? 0 : count);
+//						if (count != null)
+//							ansList.add(ans);
+//					}
+//					que.setAnswer(ansList);
+//					listMultiQue.add(que);
+//				}
+//			} else {
+//				QuestionJSON que = new QuestionJSON();
+//				que.setText(screen.getScreenText());
+//				que.setScreenId(screen.getScreenId());
+//				que.setType(chartQuestions.get(screen.getScreenId()));
+//				List<AnswerJSON> ansList = new LinkedList<AnswerJSON>();
+//				for (Long x : ansTextMap.keySet()) {
+//					Tuple<Long, Long> key = new Tuple<Long, Long>(screen.getScreenId(), x);
+//					AnswerJSON ans = new AnswerJSON();
+//					ans.setCode(x);
+//					ans.setText(ansTextMap.get(x));
+//					Integer count = ansMap.get(key);
+//					ans.setCount(count == null ? 0 : count);
+//					if (count != null)
+//						ansList.add(ans);
+//				}
+//				que.setAnswer(ansList);
+//				list.add(que);
+//			}
+//
+//		}
+//		list.addAll(listMultiQue);
+//		return list;
+//	}
 
 	private static List<GeoAnswerJSON> toGeoJsonFormat(long screenId,SurveyResult[] results) {
 		List<GeoAnswerJSON> list = new LinkedList<GeoAnswerJSON>();
@@ -420,7 +411,7 @@ public class Application extends Controller {
 			}
 		}
 		//kayıtlı olmayan mahalle kodları
-		if(townid==surveyUndefinedTownId && allDistricts !=null && allDistricts.size()>0){
+		if(townid==Integer.parseInt(session("surveyUndefinedTownId")) && allDistricts !=null && allDistricts.size()>0){
 			List<Integer> unique = new ArrayList<Integer>();
 			for(int code:allDistricts){
 				if(!unique.contains(code)){
@@ -435,16 +426,16 @@ public class Application extends Controller {
 	}
 
 	public static Result getTowns() throws SQLException {
-		if (surveyMainLocType == 2) {
+		if (Integer.parseInt(session("surveyMainLocType")) == 2) {
 			Statement st = DB.getConnection().createStatement();
-			ResultSet rs = st.executeQuery("select name from town where id ="+ surveyMainLocId);
+			ResultSet rs = st.executeQuery("select name from town where id ="+ session("surveyMainLocId"));
 			rs.next();
 			LinkedList<AddressJSON> l = new LinkedList<AddressJSON>();
-			l.add(new AddressJSON(rs.getString(1),surveyMainLocId));
+			l.add(new AddressJSON(rs.getString(1),Integer.getInteger(session("surveyMainLocId"))));
 			return ok(Json.toJson(l));
 		}
 		Statement st = DB.getConnection().createStatement();
-		ResultSet rs = st.executeQuery("select id,name from town where cityid ="+ surveyMainLocId);
+		ResultSet rs = st.executeQuery("select id,name from town where cityid ="+ session("surveyMainLocId"));
 		List<AddressJSON> l = new LinkedList<AddressJSON>();
 		while (rs.next()) {
 			l.add(new AddressJSON(rs.getString(2), rs.getInt(1)));
@@ -458,34 +449,25 @@ public class Application extends Controller {
 		if (rs == null || rs.next() == false) {
 			return ok(Json.toJson("{ \"result\":\"false\"} "));
 		} else {
-			user = username;
+			session("user", username);
 			st = DB.getConnection().createStatement();
-			rs = st.executeQuery("select s.pin,s.id,s.cp,s.locid,s.loctype,s.fromdate,s.todate,s.undefinedtownid from user u,survey s where s.usercode = u.code and u.code = '"+ user + "'");
+			rs = st.executeQuery("select s.pin,s.id,s.cp,s.locid,s.loctype,s.fromdate,s.todate,s.undefinedtownid from user u,survey s where s.usercode = u.code and u.code = '"+ session("user") + "'");
 			rs.next();
 
-			surveyPin = rs.getString(1);
-			surveyCP = rs.getString(3);
-			surveyMainLocId = rs.getInt(4);
-			surveyMainLocType = rs.getInt(5);
+			session("surveyPin",rs.getString(1));
+			session("surveyCP", rs.getString(3));
+			session("surveyMainLocId",rs.getInt(4)+"");
+			session("surveyMainLocType",rs.getInt(5)+"");
 			fromDate = rs.getDate(6);
 			toDate = rs.getDate(7);
-			surveyUndefinedTownId = rs.getInt(8); 
-			surveyId = rs.getInt(2);
+			session("surveyUndefinedTownId",""+ rs.getInt(8)); 
+			session("surveyId" ,rs.getInt(2)+"");
 			st = DB.getConnection().createStatement();
-			rs = st.executeQuery("select q.screenid,q.quetype,q.charttype from survey s, question q where q.surveyid = "+ surveyId);
-			while (rs.next()) {
-				if (rs.getInt(2) == 2) {
-					chartQuestions.put(rs.getLong(1), rs.getInt(3));
-				} else if (rs.getInt(2) == 1) {
-					descQuestions.put(rs.getLong(1), rs.getInt(3));
-				}
-			}
-			st = DB.getConnection().createStatement();
-			rs = st.executeQuery("select q.screenid from survey s, question q where q.quetype = 5 and q.surveyid = "+ surveyId);
+			rs = st.executeQuery("select q.screenid from survey s, question q where q.quetype = 5 and q.surveyId = "+ session("surveyId"));
 			rs.next();
-			filterScreenId = rs.getLong(1);
+			session("filterScreenId", ""+rs.getLong(1));
 			st = DB.getConnection().createStatement();
-			String sql = "select d.code,t.id from district d, town t where d.townid=t.id and "+(surveyMainLocType==2?"t.id =":"t.cityid =")+surveyMainLocId;
+			String sql = "select d.code,t.id from district d, town t where d.townid=t.id and "+(Integer.parseInt(session("surveyMainLocType"))==2?"t.id =":"t.cityid =")+Integer.parseInt(session("surveyMainLocId"));
 			rs = st.executeQuery(sql);
 			while (rs.next()) {
 				mahalleilce.put(rs.getInt(1), rs.getInt(2));
